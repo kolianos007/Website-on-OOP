30eb63ee4cec4110e3ff55b3d7d5fcb38be04b4b
Kol_ko <kolianos007@gmai.com>
2020-07-13 20:18:49 +0300

    prototype secondary slider

--- src/js/modules/slider/slider.js
+++ src/js/modules/slider/slider.js
@@ -1,10 +1,23 @@
 export default class Slider {
-  constructor({page = '', btns = '', prev = '', next = ''} = '') {
-    this.page = document.querySelector(page);
-    this.slides = this.page.children;
+  constructor({
+    container = null,
+    btns = null,
+    prev = null,
+    next = null,
+    activeClass = '',
+    animate,
+    autoplay
+  } = {}) {
+    this.container = document.querySelector(container);
+    this.slides = this.container.children;
     this.btns = document.querySelectorAll(btns);
+    this.prev = document.querySelector(prev);
+    this.next = document.querySelector(next);
+    this.activeClass = activeClass;
+    this.animate = animate;
+    this.autoplay = autoplay;
     this.slideIndex = 1;
   };
 
-  
+
 }
